13a8985fbd7d0dbc4cfe32cee4604f3c5a314b1b
yabu <uokom763@gmail.com>
2018-10-05 11:58:12 +0900

    first_commit

---
+++ Assets/Emergence.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class Emergence : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject obj;
+    Vector3 beforePos;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+                try
+                {
+                    beforePos = obj.transform.position;
+                    obj.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+                    obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, beforePos.z);
+                    obj.SetActive(true);
+                }
+                catch (MissingReferenceException e)
+                {
+                    Destroy(gameObject);
+                }
+
+               
+            
+        }else
+        {
+            obj.SetActive(false);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("衝突");
+        Destroy(gameObject);
+
+    }
+}
